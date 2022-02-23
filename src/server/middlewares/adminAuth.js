6ce134cd4ed64c2acaeceaef19209c6d2c35f628
Matheus Martins <matheusvmm13@gmail.com>
2022-02-23 21:22:38 +0100

    created isAdmin function

--- src/server/middlewares/adminAuth.js
+++ src/server/middlewares/adminAuth.js
@@ -1,18 +1,36 @@
 const debug = require("debug")("streaming-app:adminAuth");
 const chalk = require("chalk");
+const jwt = require("jsonwebtoken");
+const User = require("../../dataBase/models/User");
 
-const adminAuth = async (req, res, next) => {
-  const { username, admin } = req.body;
+const isAdmin = async (req, res, next) => {
+  const headerAuth = req.header("Authorization");
+  const { username } = req.body;
+  const user = User.findOne({ username });
+  debug(`User data: ${user}`);
 
-  if (admin === false) {
-    const error = new Error("Not an administrator");
-    debug(chalk.red(`Error: `, error.message));
-    res.status(403);
+  if (!headerAuth) {
+    const error = new Error("You're not authorized");
+    res.status(401);
     next(error);
   } else {
-    next();
-    debug(chalk.green(`Welcome admin: ${username}`));
+    const userToken = headerAuth.replace("Bearer ", "");
+    debug(`headerAuth: ${headerAuth}`);
+
+    if (!jwt.verify(userToken, process.env.JWT_SECRET)) {
+      const error = new Error("Unable to verify if admin");
+      res.status(401);
+      next(error);
+    } else {
+      if (user.admin === false) {
+        const error = new Error("Not an administrator");
+        debug(chalk.red(`Error: `, error.message));
+        res.status(403);
+        next(error);
+      }
+      next();
+    }
   }
 };
 
-module.exports = adminAuth;
+module.exports = isAdmin;
--- src/server/routes/platformRouter.js
+++ src/server/routes/platformRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const getPlatformsController = require("../controllers/getPlatformsController");
 const postPlatformsController = require("../controllers/postPlatformsController");
-const adminAuth = require("../middlewares/adminAuth");
+const isAdmin = require("../middlewares/adminAuth");
 
 const platformRouter = express.Router();
 
 platformRouter.get("/", getPlatformsController);
-platformRouter.post("/", adminAuth, postPlatformsController);
+platformRouter.post("/", isAdmin, postPlatformsController);
 
 module.exports = platformRouter;
