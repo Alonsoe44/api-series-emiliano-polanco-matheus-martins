4aecca2252c1d8627857775f4c2d88db757c6768
Matheus Martins <matheusvmm13@gmail.com>
2022-02-23 17:49:33 +0100

    Created database index.js

---
+++ src/dataBase/index.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-new */
+const debug = require("debug")("streaming-app:database");
+const chalk = require("chalk");
+const { default: mongoose } = require("mongoose");
+
+const connectDataBase = (connectionString) => {
+  new Promise((resolve, reject) => {
+    mongoose.set("debug", true);
+    mongoose.set("toJSON", {
+      virtuals: true,
+      transform: (doc, ret) => {
+        // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+        delete ret._id;
+        // eslint-disable-next-line no-param-reassign, no-underscore-dangle
+        delete ret.__v;
+      },
+    });
+    mongoose.connect(connectionString, (error) => {
+      if (error) {
+        debug(chalk.bold.bgMagenta.redBright(`Unable to connect Database`));
+        reject(error);
+        return;
+      }
+      debug(chalk.bold.bgMagenta.greenBright(`Database connected`));
+      resolve();
+    });
+  });
+};
+
+module.exports = connectDataBase;
