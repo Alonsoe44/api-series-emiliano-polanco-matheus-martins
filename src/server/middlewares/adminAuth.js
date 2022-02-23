32558a2b3f70f1a59a518619227256138072233d
Matheus Martins <matheusvmm13@gmail.com>
2022-02-23 22:04:07 +0100

    Refactored the isAdmin middleware

--- src/server/middlewares/adminAuth.js
+++ src/server/middlewares/adminAuth.js
@@ -1,13 +1,8 @@
-const debug = require("debug")("streaming-app:adminAuth");
-const chalk = require("chalk");
 const jwt = require("jsonwebtoken");
 const User = require("../../dataBase/models/User");
 
 const isAdmin = async (req, res, next) => {
   const headerAuth = req.header("Authorization");
-  const { username } = req.body;
-  const user = User.findOne({ username });
-  debug(`User data: ${user}`);
 
   if (!headerAuth) {
     const error = new Error("You're not authorized");
@@ -15,20 +10,14 @@ const isAdmin = async (req, res, next) => {
     next(error);
   } else {
     const userToken = headerAuth.replace("Bearer ", "");
-    debug(`headerAuth: ${headerAuth}`);
-
-    if (!jwt.verify(userToken, process.env.JWT_SECRET)) {
-      const error = new Error("Unable to verify if admin");
+    const { id } = jwt.decode(userToken, { payload: true });
+    const { admin } = await User.findById(id);
+    if (admin) {
+      next();
+    } else {
+      const error = new Error("You're not admin");
       res.status(401);
       next(error);
-    } else {
-      if (user.admin === false) {
-        const error = new Error("Not an administrator");
-        debug(chalk.red(`Error: `, error.message));
-        res.status(403);
-        next(error);
-      }
-      next();
     }
   }
 };
