36f8726347fcca9c9b13ccf74ef3b9d9d9cfd3ce
Matheus Martins <matheusvmm13@gmail.com>
2022-02-23 17:55:45 +0100

    Created errors middleware

---
+++ src/server/middlewares/errors.js
@@ -0,0 +1,17 @@
+const debug = require("debug")("streaming-app:errors");
+const chalk = require("chalk");
+
+const notFoundError = (req, res) => {
+  debug("Error: not found");
+  res.status(404).json({ error: true, message: "Resource not found" });
+};
+
+// eslint-disable-next-line no-unused-vars
+const generalError = (err, req, res, next) => {
+  debug(chalk.red(`Error: ${err.message}`));
+  const errorCode = err.code ?? 500;
+  const errorMessage = err.code ? err.message : "General error";
+  res.status(errorCode).json({ error: true, message: errorMessage });
+};
+
+module.exports = { notFoundError, generalError };
