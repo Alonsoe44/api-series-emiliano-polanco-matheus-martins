11fb715c85cded852ef336162401c1befcd8c801
Emiliano Polanco <emilianopolanco5@gmail.com>
2022-02-23 17:56:05 +0100

    Creating basic setup of the server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 require("dotenv").config();
 const debug = require("debug")("streaming-app:root");
+const startServer = require("./server/startServer");
+
+const app = require("./server/index");
 
 debug("hi");
+
+const port = process.env.PORT;
+
+(async () => {
+  try {
+    await startServer(app, port);
+  } catch {
+    debug("The server it's broken");
+  }
+})();
