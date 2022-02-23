2c0cbecccc38b26ffd30a02a05d8b12437569478
Matheus Martins <matheusvmm13@gmail.com>
2022-02-23 20:52:28 +0100

    Created the post method for platforms

--- src/server/routes/platformRouter.js
+++ src/server/routes/platformRouter.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const getPlatformsController = require("../controllers/getPlatformsController");
+const postPlatformsController = require("../controllers/postPlatformsController");
+const adminAuth = require("../middlewares/adminAuth");
 
 const platformRouter = express.Router();
 
 platformRouter.get("/", getPlatformsController);
+platformRouter.post("/", adminAuth, postPlatformsController);
 
 module.exports = platformRouter;
